8551da828724bc75eefe80d2b261d30f3602a506
Jason Adam <22adamj22@gmail.com>
2019-08-24 09:32:49 -0700

    added model notebook, preprocessing changes

---
+++ preprocess/plotting.py
@@ -0,0 +1,143 @@
+import pydicom
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage
+
+
+INPUT_FOLDER = "/Users/jasonadam/Downloads/cv_medical/scans/CQ500CT111 CQ500CT111/Unknown Study/CT PLAIN/"
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
+
+
+def load_scan(path):
+    slices = [pydicom.read_file(path + s) for s in os.listdir(path)]
+    slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(
+            slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2]
+        )
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.float)
+
+    # Set outside-of-scan pixels to 1
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+
+    if slope != 1:
+        image = slope * image.astype(np.float)
+        image = image.astype(np.float)
+
+    image += np.float(intercept)
+
+    return np.array(image, dtype=np.float)
+
+
+test_patient = load_scan(INPUT_FOLDER)
+len(test_patient)
+test_patient_pixels = get_pixels_hu(test_patient)
+
+
+# Sample Plotting
+plt.hist(test_patient_pixels.flatten(), bins=40, color="c")
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.show()
+
+# Show some slice in the middle
+plt.imshow(test_patient_pixels[13], cmap=plt.cm.gray)
+plt.show()
+
+
+def sample_stack(stack, rows=5, cols=5, start_with=0, show_every=1):
+    fig, ax = plt.subplots(rows, cols, figsize=[12, 12])
+    for i in range(rows * cols):
+        ind = start_with + i * show_every
+        ax[int(i / rows), int(i % rows)].set_title("slice {}".format(ind))
+        ax[int(i / rows), int(i % rows)].imshow(stack[ind], cmap="gray")
+        ax[int(i / rows), int(i % rows)].axis("off")
+    plt.show()
+
+
+plot_test_pixels = test_patient_pixels[1:26]
+sample_stack(plot_test_pixels)
+
+
+
+def normalize(image, min_bound=-1000.0, max_bound=2000):
+    image = (image - min_bound) / (max_bound - min_bound)
+    image[image > 1] = 1
+    image[image < 0] = 0
+    return image
+
+
+np.max(normalize(test_patient_pixels))
+np.min(normalize(test_patient_pixels))
+np.mean(normalize(test_patient_pixels))
+
+np.max(test_patient_pixels)
+np.min(test_patient_pixels)
+
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    spacing = [float(scan[0].SliceThickness)]
+    pixel_spacing = [float(i) for i in scan[0].PixelSpacing]
+    for i in pixel_spacing:
+        spacing.append(i)
+
+    spacing = np.array(spacing)
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+    
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+    
+    return image
+
+
+resamp_test = resample(test_patient_pixels, test_patient)
+resamp_test.shape
+
+
+def plot_3d(image, threshold=-300):
+    
+    # Position the scan upright, 
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2,1,0)
+    
+    verts, faces = measure.marching_cubes(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
--- preprocess/process_dicom.py
+++ preprocess/process_dicom.py
@@ -2,12 +2,33 @@ import pydicom
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
+from collections import Counter
+import pandas as pd
+import scipy.ndimage
 
 INPUT_FOLDER = "/Users/jasonadam/Downloads/cv_medical/scans/"
+SAVE_FOLDER = "/Users/jasonadam/Downloads/cv_medical/processed_arrays/"
+LABELS_PATH = "/Users/jasonadam/Downloads/cv_medical/ich_reads.csv"
+
+labels = pd.read_csv(LABELS_PATH)
+labels.head()
+
+slice_counts = []
+for ct in os.listdir(INPUT_FOLDER):
+    images_path = os.path.join(INPUT_FOLDER, ct + "/" + "Unknown Study/")
+    list_scans = os.listdir(images_path)
+
+    for scan_folders in list_scans:
+        slice_path = os.path.join(images_path, scan_folders + "/")
+        slices = os.listdir(slice_path)
+        slice_counts.append(len(slices))
+
+plt.hist(slice_counts)
+counts = Counter(slice_counts)
+counts.most_common(1)
 
 
-def get_loaded_scans(base_path, slice_count: int = 256):
+def get_loaded_scans(base_path, slice_count: int = 32):
     for ct in os.listdir(base_path):
         images_path = os.path.join(base_path, ct + "/" + "Unknown Study/")
         list_scans = os.listdir(images_path)
@@ -22,15 +43,24 @@ def get_loaded_scans(base_path, slice_count: int = 256):
                 yield list([pydicom.read_file(os.path.join(slice_path, s)) for s in slices])
 
 
-# test = get_loaded_scans(base_path=INPUT_FOLDER)
-# for patient in test:
-#     for i in patient:
-#         print(i.PatientName)
+test = get_loaded_scans(base_path=INPUT_FOLDER)
+patient_ids = []
+for patient in test:
+    patient_ids.append(patient[0].PatientName)
+
+patient_ids = [str(i) for i in patient_ids] 
+patient_ids = pd.DataFrame(patient_ids)
+patient_ids
+
+labels = labels[labels["patient_id"].isin(patient_ids)].reset_index()
+labels.drop(columns="index", inplace=True, axis=1)
+labels
 
 
 def get_pixels_hu(slices: list):
     for scans in slices:
         try:
+            patient_id = str(scans[0].PatientName)
             image = np.stack([s.pixel_array for s in scans])
             image = image.astype(np.float)
             # Set outside-of-scan pixels to 0
@@ -48,39 +78,35 @@ def get_pixels_hu(slices: list):
 
                 image[slice_number] += np.float(intercept)
 
-            yield np.array(image, dtype=np.float)
-        except OSError:
+            yield np.array(image, dtype=np.float), patient_id
+        except:
             pass
 
 
-# test = get_loaded_scans(base_path=INPUT_FOLDER, slice_count=32)
-# arrays_test = get_pixels_hu(test)
-# for i in arrays_test:
-#     print(i.shape)
+test = get_loaded_scans(base_path=INPUT_FOLDER, slice_count=32)
+arrays_test = get_pixels_hu(test)
+
+for stack, patient_name, scans in arrays_test:
+    print(stack.shape, patient_name, scans[0].SliceThickness)
+
+len(os.listdir(SAVE_FOLDER))
 
 
+def normalize_stacks(image, min_bound=-1000.0, max_bound=2000.0):
+    image = (image - min_bound) / (max_bound - min_bound)
+    image[image > 1] = 1
+    image[image < 0] = 0
+    return image
+
+from skimage.transform import rescale
+
+test = get_loaded_scans(base_path=INPUT_FOLDER, slice_count=32)
+arrays_test = get_pixels_hu(test)
+for i, j in arrays_test:
+    normalized = scipy.ndimage.interpolation.zoom(i, (0.25, 0.25, 0.25), mode="nearest")
+    normalized = normalize_stacks(normalized)
+    normalized = normalized - np.mean(normalized)
+    np.save(os.path.join(SAVE_FOLDER, j), normalized)
 
 
-
-# Sample Plotting
-plt.hist(first_patient_pixels.flatten(), bins=40, color="c")
-plt.xlabel("Hounsfield Units (HU)")
-plt.ylabel("Frequency")
-plt.show()
-
-# Show some slice in the middle
-plt.imshow(first_patient_pixels[30], cmap=plt.cm.gray)
-plt.show()
-
-
-def sample_stack(stack, rows=6, cols=6, start_with=1, show_every=1):
-    fig, ax = plt.subplots(rows, cols, figsize=[12, 12])
-    for i in range(rows * cols):
-        ind = start_with + i * show_every
-        ax[int(i / rows), int(i % rows)].set_title("slice {}".format(ind))
-        ax[int(i / rows), int(i % rows)].imshow(stack[ind], cmap="gray")
-        ax[int(i / rows), int(i % rows)].axis("off")
-    plt.show()
-
-
-sample_stack(pixes_hu)
+os.listdir(SAVE_FOLDER)
